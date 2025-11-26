72c21b55f79374c1505ebc62e69dcfddc6e17b92
MyKnow <myknow000@gmail.com>
2025-11-26 16:35:17 +0900

    feat: 다국어 지원을 위한 easy_localization 구성
    - easy_localization: ^3.0.8
    - en.json 및 ko.json 생성
    - iOS 지원을 위해 Info.plist 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ko')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('ko'),
+      child: const MainApp(),
+    ),
+  );
 }
 
 /// The main application widget.
@@ -11,8 +22,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: Scaffold(body: Center(child: Text('test-text'.tr()))),
     );
   }
 }
