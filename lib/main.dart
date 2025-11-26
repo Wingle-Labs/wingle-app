3b1602d0eb87665aca9963226c54e5c48b10fd70
MyKnow <myknow000@gmail.com>
2025-11-27 02:29:21 +0900

    rename: localization.dart.dart의 파일명 변경
    - localization.dart.dart -> localization.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:value_date/app/config/localization.dart.dart';
+import 'package:value_date/app/config/localization.dart';
 
 import 'app/app.dart';
 
