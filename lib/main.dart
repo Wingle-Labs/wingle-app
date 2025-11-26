5a795a74b23ce2ba7cb215091b893b5edced3ffb
MyKnow <myknow000@gmail.com>
2025-11-26 16:47:55 +0900

    feat: flutter_riverpod 초기 구성
    - flutter_riverpod: ^3.0.3
    - custom_lint: ^0.8.1
    - riverpod_lint: ^3.0.3
    - main.dart에 ProviderScope 구성 완료
    - anlysis_options.yaml에 custom_lint 선언 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
   runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('ko')],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('ko'),
-      child: const MainApp(),
+    ProviderScope(
+      child: EasyLocalization(
+        supportedLocales: const [Locale('en'), Locale('ko')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('ko'),
+        child: MainApp(),
+      ),
     ),
   );
 }
 
 /// The main application widget.
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   /// The main application widget.
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
