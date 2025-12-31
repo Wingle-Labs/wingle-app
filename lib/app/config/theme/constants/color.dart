169e06db4ecceb0a2111356bf74104e0fa8a0396
myknow <myknow000@gmail.com>
2025-12-31 17:44:38 +0900

    feat: AppColorScheme 추가

--- lib/app/config/theme/constants/color.dart
+++ lib/app/config/theme/constants/color.dart
@@ -1,5 +1,77 @@
 import 'package:flutter/material.dart';
 
+/// 디자인 시스템의 색상 스킴을 정의하는 인터페이스
+abstract interface class AppColorScheme {
+  /// 주요 색상 (Primary color)
+  Color get primary;
+
+  /// 주요 색상에 대한 텍스트 색상 (On-primary color)
+  Color get onPrimary;
+
+  /// 주요 텍스트 색상 (Primary text color)
+  Color get primaryText;
+
+  /// 보조 텍스트 색상 (Secondary text color)
+  Color get secondaryText;
+
+  /// 제3 텍스트 색상 (Tertiary text color)
+  Color get tertiaryText;
+
+  /// 비활성화 및 입력 불가 텍스트 색상
+  Color get disabledText;
+
+  /// 비활성화 배경 색상
+  Color get disabledBackground;
+
+  /// 강한 비활성화 텍스트 색상
+  Color get strongDisabledText;
+
+  /// 강한 비활성화 배경 색상
+  Color get strongDisabledBackground;
+
+  /// 배경 색상
+  Color get background;
+
+  /// 표면 색상 (Surface color)
+  Color get surface;
+
+  /// 표면 변형 색상 (Surface variant color)
+  Color get surfaceVariant;
+
+  /// Surface Elevated 색상
+  Color get surfaceElevated;
+
+  /// 테두리 색상 (Border or Outline color)
+  Color get border;
+
+  /// 구분선 색상 (Divider color)
+  Color get divider;
+
+  /// State Default 색상
+  Color get stateDefault;
+
+  /// State Pressed 색상
+  Color get statePressed;
+
+  /// State Disabled 색상
+  Color get stateDisabled;
+
+  /// Pimary Scale 90
+  Color get primaryScale90;
+
+  /// Pimary Scale 70
+  Color get primaryScale70;
+
+  /// Pimary Scale 50
+  Color get primaryScale50;
+
+  /// Pimary Scale 30
+  Color get primaryScale30;
+
+  /// Pimary Scale 10
+  Color get primaryScale10;
+}
+
 /// 어플리케이션의 공통 색상 정의
 class AppColor {
   /// Light Mode 주요 색상
