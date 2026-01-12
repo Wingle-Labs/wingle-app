1b7988bd703979012caa0ce3af21cca05f53eda7
myknow <myknow000@gmail.com>
2026-01-13 00:05:50 +0900

    feat: 캐러셀 컴포넌트 생성
    - <Widget, Image> 형태의 Map을 받는 StatelessWidget
    - 부모 객체로부터 controller를 전달 받아 사용
    - index 관리도 부모 객체가 담당

---
+++ lib/features/onboarding/presentation/components/wrapper/indicator_carousel.dart
@@ -0,0 +1,66 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:wingle/app/config/theme/components/states/indicator.dart';
+import 'package:wingle/app/config/theme/constants/size.dart';
+import 'package:wingle/app/config/theme/constants/spacing.dart';
+
+/// Indicator와 Carousel을 포함한 Widget
+class IndicatorCarousel extends StatelessWidget {
+  /// Carousel의 아이템들
+  final List<MapEntry<Widget, ImageProvider>> items;
+
+  /// Carousel의 컨트롤러
+  final CarouselSliderController controller;
+
+  /// 현재 인덱스
+  final int currentIndex;
+
+  /// 인덱스 변경시 호출되는 콜백
+  final ValueChanged<int> onIndexChanged;
+
+  /// const 생성자
+  const IndicatorCarousel({
+    super.key,
+    required this.items,
+    required this.controller,
+    required this.currentIndex,
+    required this.onIndexChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Indicator(currentIndex: currentIndex, length: items.length),
+        CarouselSlider(
+          carouselController: controller,
+          items: items.map((entry) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: AppContainerSize.indicatorDescription,
+                  child: Center(child: entry.key),
+                ),
+                const SizedBox(height: AppSpacing.xs),
+                SizedBox(
+                  height: AppContainerSize.indicatorImage,
+                  width: AppContainerSize.indicatorImage,
+                  child: Image(image: entry.value, fit: BoxFit.cover),
+                ),
+              ],
+            );
+          }).toList(),
+          // TODO: 매직 넘버 제거
+          options: CarouselOptions(
+            aspectRatio: 1 / 1.5,
+            viewportFraction: 0.9,
+            enlargeCenterPage: true,
+            enableInfiniteScroll: false,
+            onPageChanged: (index, _) => onIndexChanged(index),
+          ),
+        ),
+      ],
+    );
+  }
+}
