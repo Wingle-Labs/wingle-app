d7f4094adeb0fe1753dcd7898bdad4b0a5e6d769
myknow <myknow000@gmail.com>
2026-01-13 00:01:12 +0900

    feat: DefaultIntruction 수정
    - font weight: bold로 변경
    - textAlign 파라미터 추가

--- lib/app/config/theme/components/texts/default_intruction.dart
+++ lib/app/config/theme/components/texts/default_intruction.dart
@@ -3,20 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wingle/app/config/theme/constants/padding.dart';
 import 'package:wingle/app/config/theme/constants/size.dart';
+import 'package:wingle/app/config/theme/constants/weight.dart';
 
 /// 기본 설명 텍스트
 class DefaultInstruction extends ConsumerWidget {
   /// localization 키
   final String text;
 
+  /// 텍스트 정렬
+  final TextAlign textAlign;
+
   /// 생성자
-  const DefaultInstruction(this.text, {super.key});
+  const DefaultInstruction(this.text, {super.key, this.textAlign = .left});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: .symmetric(vertical: AppPadding.vertical),
-      child: Text(text.tr(), style: TextStyle(fontSize: AppFontSize.title)),
+      child: Text(
+        text.tr(),
+        style: TextStyle(
+          fontSize: AppFontSize.title,
+          fontWeight: AppFontWeight.bold,
+        ),
+        textAlign: textAlign,
+      ),
     );
   }
 }
