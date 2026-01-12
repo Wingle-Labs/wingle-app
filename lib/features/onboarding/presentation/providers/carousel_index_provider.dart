27bf7b914d1fea930a4ec55b86464b8ccb9fd79a
myknow <myknow000@gmail.com>
2026-01-13 00:23:52 +0900

    feat: Carousel Provider 변경
    - controller도 함께 관리하도록 NotifierProvider로 변경함

--- lib/features/onboarding/presentation/providers/carousel_index_provider.dart
+++ lib/features/onboarding/presentation/providers/carousel_index_provider.dart
@@ -1,20 +1,37 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'carousel_index_provider.g.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Carousel의 현재 인덱스를 관리하는 Provider
-@riverpod
-class CarouselIndex extends _$CarouselIndex {
-  @override
-  int build() => 0;
+final onboardingCarouselProvider =
+    NotifierProvider<OnboardingCarouselNotifier, int>(
+      OnboardingCarouselNotifier.new,
+    );
 
-  /// Carousel의 현재 인덱스를 업데이트하는 메소드
-  void updateIndex(int index) {
-    state = index;
+/// Carousel의 현재 인덱스를 관리하는 Notifier
+class OnboardingCarouselNotifier extends Notifier<int> {
+  /// CarouselSliderController
+  late final CarouselSliderController controller;
+
+  @override
+  int build() {
+    controller = CarouselSliderController();
+    return 0;
   }
 
-  /// Carousel의 현재 인덱스를 증가하는 메소드
-  void incrementIndex() {
-    state++;
+  /// 다음 페이지로 이동
+  void next() {
+      state++;
+      controller.animateToPage(state);
+  }
+
+  /// 마지막 페이지로 이동
+  void skipToLast(int lastIndex) {
+    state = lastIndex;
+    controller.animateToPage(lastIndex);
+  }
+
+  /// 인덱스 업데이트
+  void update(int index) {
+    state = index;
   }
 }
