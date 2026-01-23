2e2f9953714353477740db0d9d397192decb9f41
myknow <myknow000@gmail.com>
2026-01-24 01:45:05 +0900

    feat: TextScaleWrapper 추가

---
+++ lib/app/config/theme/components/texts/text_scale_wrapper.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:wingle/app/config/theme/components/texts/text_scale_policy.dart';
+
+/// 시스템 텍스트 크기 정책에 따른 UI 내 텍스트 크기 조정 Wrapper
+class TextScaleWrapper extends StatelessWidget {
+  /// 내부 UI
+  final Widget child;
+
+  /// 텍스트 크기 정책
+  final TextScalePolicy policy;
+
+  /// const 생성자
+  const TextScaleWrapper({
+    required this.child,
+    this.policy = TextScalePolicy.system,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    double scale = mediaQuery.textScaler.scale(1.0);
+
+    switch (policy) {
+      case TextScalePolicy.system:
+        break;
+      case TextScalePolicy.cappedSmall:
+        scale = scale.clamp(1.0, 1.1);
+        break;
+      case TextScalePolicy.cappedMedium:
+        scale = scale.clamp(1.0, 1.3);
+        break;
+      case TextScalePolicy.cappedLarge:
+        scale = scale.clamp(1.0, 1.6);
+        break;
+      case TextScalePolicy.fixed:
+        scale = 1.0;
+        break;
+    }
+
+    return MediaQuery(
+      data: mediaQuery.copyWith(textScaler: TextScaler.linear(scale)),
+      child: child,
+    );
+  }
+}
