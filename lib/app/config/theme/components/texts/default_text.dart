a1bfefdda0d858d4093fbfd3bb36cfecc4b10c6d
myknow <myknow000@gmail.com>
2026-01-24 22:38:02 +0900

    feat: DefaultText 컴포넌트 생성

---
+++ lib/app/config/theme/components/texts/default_text.dart
@@ -0,0 +1,38 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:wingle/app/config/theme/components/texts/text_scale_policy.dart';
+import 'package:wingle/app/config/theme/components/texts/text_scale_wrapper.dart';
+import 'package:wingle/common/extensions/context_typography.dart';
+
+/// 기본 텍스트 컴포넌트
+class DefaultText extends StatelessWidget {
+  /// 텍스트 내용
+  final String text;
+
+  /// 텍스트 스케일 정책
+  final TextScalePolicy policy;
+
+  /// 텍스트 스타일
+  final TextStyle? style;
+
+  /// 텍스트 정렬
+  final TextAlign textAlign;
+
+  /// 생성자
+  const DefaultText(
+    this.text, {
+    super.key,
+    this.policy = TextScalePolicy.system,
+    this.style,
+    this.textAlign = TextAlign.left,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final body = context.typography.body;
+    return TextScaleWrapper(
+      policy: policy,
+      child: Text(text.tr(), style: style ?? body, textAlign: textAlign),
+    );
+  }
+}
