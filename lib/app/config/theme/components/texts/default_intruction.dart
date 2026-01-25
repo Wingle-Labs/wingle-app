b4b03e823af7c691e7c012d0a31b4e1be716928a
myknow <myknow000@gmail.com>
2026-01-25 16:17:34 +0900

    feat: DefaultIntruction Padding 변경

--- lib/app/config/theme/components/texts/default_intruction.dart
+++ lib/app/config/theme/components/texts/default_intruction.dart
@@ -12,14 +12,22 @@ class DefaultInstruction extends ConsumerWidget {
   /// 텍스트 정렬
   final TextAlign textAlign;
 
+  /// padding
+  final EdgeInsets padding;
+
   /// 생성자
-  const DefaultInstruction(this.text, {super.key, this.textAlign = .left});
+  const DefaultInstruction(
+    this.text, {
+    super.key,
+    this.textAlign = .left,
+    this.padding = const EdgeInsets.symmetric(vertical: AppPadding.vertical),
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final titleStyle = context.typography.title;
     return Padding(
-      padding: .symmetric(vertical: AppPadding.vertical),
+      padding: padding,
       child: DefaultText(
         text,
         style: titleStyle,
