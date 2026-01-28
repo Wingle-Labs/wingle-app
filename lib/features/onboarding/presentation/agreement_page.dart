5dac429aa0f31384bcd3bbbe1aaf24e7b64d01ee
myknow <myknow000@gmail.com>
2026-01-28 15:36:43 +0900

    feat: AgreementPage 및 DefaultListButton 임시 구현

--- lib/features/onboarding/presentation/agreement_page.dart
+++ lib/features/onboarding/presentation/agreement_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wingle/app/config/theme/components/bottons/default_checkbox.dart';
 import 'package:wingle/app/config/theme/components/bottons/default_filled_button.dart';
+import 'package:wingle/app/config/theme/components/bottons/default_list_button.dart';
 import 'package:wingle/app/config/theme/components/cards/guide_card.dart';
 import 'package:wingle/app/config/theme/components/wrappers/constrained_scrollable_scaffold.dart';
 import 'package:wingle/app/config/theme/components/wrappers/default_app_bar.dart';
@@ -27,16 +29,39 @@ class AgreementPage extends ConsumerWidget {
             message: "얼마 전에 소녀 앞에서 한 번 실수를 했을 뿐,\n여태 큰길 가듯이 건너던 징검다리",
           ),
           Container(
-            height: 480,
             padding: .only(
               top: AppPadding.listTop,
               bottom: AppPadding.listBottom,
             ),
-            child: ListView.builder(
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return const SizedBox();
-              },
+            child: Column(
+              children: [
+                DefaultCheckbox(isChecked: true, onChanged: (value) {}),
+                DefaultListButton(
+                  isChecked: true,
+                  onChanged: (value) {},
+                  isDisabled: false,
+                  label: "전체 동의",
+                  hasArrow: false,
+                ),
+                DefaultListButton(
+                  isChecked: true,
+                  onChanged: (value) {},
+                  isDisabled: false,
+                  label: "윙글 이용약관 필수동의",
+                ),
+                DefaultListButton(
+                  isChecked: false,
+                  onChanged: (value) {},
+                  isDisabled: false,
+                  label: "개인정보 처리 동의",
+                ),
+                DefaultListButton(
+                  isChecked: false,
+                  onChanged: (value) {},
+                  isDisabled: true,
+                  label: "위치정보 이용 동의",
+                ),
+              ],
             ),
           ),
         ],
